b2e200f45cc6d8bb798181d2fd87c6b51186c17f
subhash928 <subhash928@gmail.com>
2023-06-10 17:29:20 -0500

    restarting the project

--- personal-portfolio/src/app/app.component.spec.ts
+++ personal-portfolio/src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-portfolio'`, () => {
+  it(`should have as title 'personal-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-portfolio');
+    expect(app.title).toEqual('personal-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project-portfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('personal-portfolio app is running!');
   });
 });
