b464891f07474cae3aa8a3821a77230dd0687acd
Subhash Pasumarthi <subhash@Subhashs-MacBook-Pro.local>
2024-02-21 16:30:36 -0600

    added assets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,6 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
 
 export const NavBar = () => {
 
